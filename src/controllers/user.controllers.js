8bb810cc06740a56dfc3a60c54596ffa574e9b0a
Shristi <sethiyashristi20@gmail.com>
2024-02-14 00:14:52 +0530

    Added Register Controller

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,10 +1,79 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import {ApiError} from "../utils/ApiError.js"
+import { User } from "../models/user.models.js";
+import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import { ApiResponse } from "../utils/ApiResponse.js";}
 
 
 const registerUser = asyncHandler(async (req,res) =>{
-    res.status(200).json({
-        message : "ok"
-    })
+   //steps 
+   //step1  get user details from frontend (through postman for now)
+    
+   const {fullname,email,username,password} = req.body
+   
+
+   //step2 validation of details -not empty and other
+
+//    if(fullname===""){
+//     throw new ApiError(400,"FullName is required")
+//    }
+
+  if([fullname,email,username,password].some((field)=>field?.trim()==="")){
+    throw new ApiError(400,"All fields are required")
+  }
+
+   //step3 check if user already exists via email/username
+
+   const existedUser = User.findOne({
+    $or: [{ email },{ username }]
+   })
+  if(existedUser){
+    throw new ApiError(409,"User already exists!")
+  }
+   //step4 check for images, check for avatar
+
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath= req?.files?.coverImage[0]?.path
+
+    if(!avatarLocalPath){
+        throw new ApiError(400,"Avatar is required!")
+    }
+
+   //step5 upload on cloudinary
+
+ const avatar = await uploadOnCloudinary(avatarLocalPath)
+ const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+  
+if(!avatar){
+    throw new ApiError(400,"Avatar is required!")
+}
+
+//create user object (to send to mongodb) || create entry in db
+
+const user = await User.create({
+    fullname,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    password,
+    username: username.toLowerCase()
+})
+
+//remove password and refresh token field from response
+   const createdUser = user.findById(user._id).select(
+    "-password -refreshToken" ,//dont want these fiels
+)
+
+//check for user creation
+   if(!createdUser){
+    throw new ApiError(500,"Something went wrong while registering the user!")
+   }
+
+
+//return response 
+return res.status(201).json(
+    new ApiResponse(200,createdUser, "User Registered Successfully!!")
+)
+
 })
 
 export {registerUser}
--- src/middlewares/multer.middlewares.js
+++ src/middlewares/multer.middlewares.js
@@ -8,7 +8,7 @@ const storage = multer.diskStorage({
       cb(null, file.originalname)
     }
   })
-  
+   
  export const upload = multer({
     storage,
  }) 
